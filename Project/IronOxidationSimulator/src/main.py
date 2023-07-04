9525f79b979e5c9391a0e8140e9d1232214e1237
Ding <51362632+edsml-dd1522@users.noreply.github.com>
2023-07-04 14:20:01 +0800

    Daily Progress 04/07

--- Project/IronOxidationSimulator/src/main.py
+++ Project/IronOxidationSimulator/src/main.py
@@ -1,10 +1,33 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QVBoxLayout, QLabel, QWidget, QAction
 import os
 import subprocess
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setFixedSize(1024, 768)
+
+        # Create a menubar
+        self.menu = self.menuBar()
+
+        # Add menu items
+        self.info_menu = self.menu.addMenu("信息")
+
+        # Create actions for menu items
+        self.show_welcome_action = QAction("显示欢迎", self)
+        self.show_welcome_action.triggered.connect(self.show_welcome)
+
+        # Add actions to menu items
+        self.info_menu.addAction(self.show_welcome_action)
+
+        self.setWindowTitle("主窗口")
+
+    def show_welcome(self):
+        self.welcome_window = WelcomeWindow(self)
+        self.welcome_window.show()
 
 class WelcomeWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -16,12 +39,6 @@ class WelcomeWindow(QMainWindow):
         # 设置窗口标题
         self.setWindowTitle("Welcome!")
 
-        # 显示软件名称
-
-        # 显示日期和版本号
-
-        # 将确认按钮与open_main_window方法连接
-        self.EntryButton.clicked.connect(self.open_main_window)
 
         # 将退出按钮与close方法连接
         self.ExitButton.clicked.connect(self.close)
@@ -29,24 +46,24 @@ class WelcomeWindow(QMainWindow):
         # 连接Manual按钮的clicked信号到open_pdf_manual方法
         self.Manual.clicked.connect(self.open_pdf_manual)
 
-    def open_main_window(self):
-        # 创建主窗口
-        # 这里我们没有真正的主窗口，所以我们只创建了一个空的QMainWindow对象
-        self.main_window = QMainWindow()
-        self.main_window.show()
-        self.close()
-
     def open_pdf_manual(self):
         pdf_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "assets", "manual.pdf")
+        if not os.path.isfile(pdf_path):
+            QMessageBox.critical(self, "File Not Found",
+                                 "The manual file does not exist.  Please check the GitHub page or reinstall the "
+                                 "software.",
+                                 QMessageBox.Ok)
+            return
         if sys.platform.startswith('darwin'):  # macOS
             subprocess.call(('open', pdf_path))
-        elif sys.platform.startswith('linux'):  # linux
+        elif sys.platform.startswith('linux'):  # Linux
             subprocess.call(('xdg-open', pdf_path))
         elif sys.platform.startswith('win32'):  # Windows
             os.startfile(pdf_path)
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    welcome_window = WelcomeWindow()
-    welcome_window.show()
+    main_window = MainWindow()
+    main_window.show()
     sys.exit(app.exec_())
